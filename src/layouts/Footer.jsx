4a7d65f9d7d06267708d056e8fece335d3dfa245
hasib1010 <asifmahmud664@gmail.com>
2024-01-16 00:08:28 +0600

    dark mode fixed

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-black'>
-            Footer Section
+        <div className='bg-black text-white '>
+            <div className='container mx-auto min-h-20'>
+               <div>
+               <h2 className='text-3xl font-semibold italic pt-6'>MD. Hasibul Hasan</h2>
+                <p className='italic'>Dhaka, Bangladesh</p>
+                <p>+880 1608 471414</p>
+               </div>
+            </div>
         </div>
     );
 };
